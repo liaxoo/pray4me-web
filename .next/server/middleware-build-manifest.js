bc425ad07e0bc3917f66300d1813402ad5956f75
Liaxo <57733867+liaxoo@users.noreply.github.com>
2026-01-09 17:41:55 -0500

    dx

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,1 +1,22 @@
-globalThis.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-42372ed130431b0a.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-30ee026fa6d1a4a9.js","static/chunks/4bd1b696-979a7f9183e6ff82.js","static/chunks/1684-a185d1e2bb42e61c.js","static/chunks/main-app-ac9d9446a6ad4fd0.js"],rootMainFilesTree:{},pages:{"/_app":["static/chunks/webpack-30ee026fa6d1a4a9.js","static/chunks/framework-6c1e25edaa207523.js","static/chunks/main-8f20077c90111098.js","static/chunks/pages/_app-5d1abe03d322390c.js"],"/_error":["static/chunks/webpack-30ee026fa6d1a4a9.js","static/chunks/framework-6c1e25edaa207523.js","static/chunks/main-8f20077c90111098.js","static/chunks/pages/_error-3b2a1d523de49635.js"]},ampFirstPages:[]},globalThis.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
+globalThis.__BUILD_MANIFEST = {
+  "polyfillFiles": [
+    "static/chunks/polyfills.js"
+  ],
+  "devFiles": [],
+  "ampDevFiles": [],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/webpack.js",
+    "static/chunks/main-app.js"
+  ],
+  "rootMainFilesTree": {},
+  "pages": {
+    "/_app": []
+  },
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
